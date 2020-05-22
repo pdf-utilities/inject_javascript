dcde7fd392b62bd17362a31cde1872f43ef65431
S0AndS0 <strangerthanbland@gmail.com>
2020-05-22 15:37:22 -0700

    :snake: Adjusts import declarations

--- inject_javascript/lib/__init__.py
+++ inject_javascript/lib/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 
-from inspect import currentframe as Inspect_currentframe
+from inspect import currentframe
 
 
 def notice(message):
     """
     Custom warning notifier
     """
-    caller = Inspect_currentframe().f_back.f_code.co_name
-    lineno = Inspect_currentframe().f_back.f_lineno
+    caller = currentframe().f_back.f_code.co_name
+    lineno = currentframe().f_back.f_lineno
     msg = 'Notice: {0} {1}\n\t{2}'.format(lineno, caller, message)
     print(msg)
 
@@ -18,7 +18,7 @@ def error(message):
     """
     Cusotm Exception _thrower_
     """
-    caller = Inspect_currentframe().f_back.f_code.co_name
-    lineno = Inspect_currentframe().f_back.f_lineno
+    caller = currentframe().f_back.f_code.co_name
+    lineno = currentframe().f_back.f_lineno
     msg = '\n\t{0} {1} reports: {2}'.format(lineno, caller, message)
     raise Exception(msg)
