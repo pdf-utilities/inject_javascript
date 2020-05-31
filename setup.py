29587c289ee4c4bc37cb80f7939abc1c9a5ea3fe
S0AndS0 <strangerthanbland@gmail.com>
2020-05-30 23:47:26 -0700

    :arrow_up: Bumps version number repackaging

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(".github/README.md", "r") as fh:
 
 setup(
     name = "inject_javascript",
-    version = "0.0.6",
+    version = "0.0.7",
     author = "S0AndS0",
     author_email = "StrangerThanBland@gmail.com",
     description = "Inject JavaScript within PDF document body",
